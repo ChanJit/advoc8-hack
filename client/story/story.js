963bb10dcf02f8769cb5f64f40b5d30bca7b2868
Tze Wei <tzeweiwee@gmail.com>
2017-07-30 03:18:59 +0800

    completed the main functionalities of the story

--- client/story/story.js
+++ client/story/story.js
@@ -2,7 +2,7 @@ import styles from './story.css';
 
 var stories = [
     { id: 0, line: "Use earphone to optimize the experience" },
-    { id: 1, line: "Chinh, Vietnamese, 24" }
+    { id: 1, line: "Chinh, Vietnamese, 24" },
     // { id: 2, line: "The boat was rocking so hard that Chinh jerked awake. It was dark. He couldn’t see anything but he could hear the breathing sound of humans around him. " },
     // { id: 3, line: "In a far corner, he could even hear a wheezing sound that looks like it belonged to an elderly man." },
     // { id: 4, line: "“Where am I?”" },
@@ -14,8 +14,11 @@ var stories = [
     // { id: 10, line: "While Chinh was lost in his own thoughts, there was a commotion among the others. A tall silhouette had appeared demanding them to hand over their passports for a checkpoint ahead." },
     // { id: 11, line: "That deep voice continued on that he will be keeping their passport from this point onwards and will only get back their passport when their contracts with their employers have ended." },
     // { id: 12, line: "Someone protested and the man sneered, “This is how things work here, if you don’t like it, you can leave now and pay us the money for your travels here.”" },
-    // { id: 13, line: "The crowd went silent. Fear shot through Chinh. “I can’t leave now, how would Mama and Phuong survive?”" }
+    // { id: 13, line: "The crowd went silent. Fear shot through Chinh. “I can’t leave now, how would Mama and Phuong survive?”" },
+     { id: 2, line: "Beware of every jobs you're going to apply. Human trafficking is real. Don't be a victim. Protect your family, friends and community from scams."}
 ];
+
+var storySessionDone = false;
 
 Tracker.autorun(function () {
     var sessionVal = Session.get("userProfileDone");
@@ -48,6 +51,8 @@ function storyTeller() {
 
 function displayStory(id){
     if(id > stories.length-1){
+        storySessionDone = true;
+        displayDecision();
         return;
     }
     let target = $("#"+id);
@@ -68,5 +73,17 @@ function removeStory(id){
         target.css("display", "none");
         displayStory(id+1);
     }, 500);
+}
+
+function displayDecision(){
+    if(storySessionDone){
+        var removeID = stories.length -1;
+        $("#"+removeID).css("display","none");
+        window.setTimeout(function(){
+            $('._client_story__story__userDecision').css("display","block");
+        }, 1000);
+        
+    }
+    
 }
 
