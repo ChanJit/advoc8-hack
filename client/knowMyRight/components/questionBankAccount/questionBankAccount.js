f697616e98c89ef2fb86dee7fa5128501352f3e9
Jun Kit <jkchan@seekasia.com>
2017-07-29 23:07:28 +0800

    conclusion done

--- client/knowMyRight/components/questionBankAccount/questionBankAccount.js
+++
@@ -1,7 +0,0 @@
-Template.questionBankAccount.events({
-  "click #btnNext": function (e) {
-    e.preventDefault();
-    Session.set("questionBankAccount",false);
-    Session.set("questionFreedom",true);
-  }
-})
--- client/knowMyRight/components/questionMedicalClaim/questionMedicalClaim.js
+++
@@ -1,7 +0,0 @@
-Template.questionMedicalClaim.events({
-  "click #btnNext": function (e) {
-    e.preventDefault();
-    Session.set("questionMedicalClaim",false);
-    Session.set("questionMeal",true);
-  }
-})
