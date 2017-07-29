f390764ee45fa340495ba031a90f9dbb09915ec0
Jun Kit <jkchan@seekasia.com>
2017-07-29 21:16:37 +0800

    last question done

--- client/knowMyRight/components/questionFreedom/questionFreedom.js
+++ client/knowMyRight/components/questionFreedom/questionFreedom.js
@@ -1,3 +1,16 @@
+import styles from './questionFreedom.css';
+
+import {
+  YES_NO_LIST
+} from '../../../components/constants/FormData';
+
+Template.questionFreedom.helpers({
+  styles: styles,
+  yesNoList: function(){
+    return YES_NO_LIST;
+  }
+});
+
 Template.questionFreedom.events({
   "click #btnNext": function (e) {
     e.preventDefault();
--- client/knowMyRight/components/questionPayment/questionPayment.js
+++ client/knowMyRight/components/questionPayment/questionPayment.js
@@ -29,6 +29,6 @@ Template.questionPayment.events({
   "click #btnNext": function (e) {
     e.preventDefault();
     Session.set("questionPayment",false);
-    Session.set("questionBankAccount",true);
+    Session.set("questionFreedom",true);
   }
 })
