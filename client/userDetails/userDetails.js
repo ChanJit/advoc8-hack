e93ce58e80bdc040d857cf8ce37d3a7702a4490f
Tze Wei <tzeweiwee@gmail.com>
2017-07-30 02:20:01 +0800

    added story, removed moon css.

--- client/userDetails/userDetails.js
+++ client/userDetails/userDetails.js
@@ -1,24 +1,28 @@
 import styles from './userDetails.css';
 
 var userSections = [
-    "._client_story__story__detailsSectionA",
-    "._client_story__story__detailsSectionB",
-    "._client_story__story__detailsSectionC"
+    "._client_userDetails__userDetails__detailsSectionA",
+    "._client_userDetails__userDetails__detailsSectionB",
+    "._client_userDetails__userDetails__detailsSectionC"
 ];
 
-var screenPos = 200;
+var screenPos = 160;
 
 //start user screening position
 window.onload = function(){
     $(document).scrollTop(screenPos);
 }
 
+Template.userDetails.created = function(){
+    Session.setDefault("userProfileDone", false);
+};
+
 Template.userDetails.helpers({
     styles:styles
 });
 
 Template.userDetails.events({
-    "click ._client_story__story__detailsSectionA button": function(e) {
+    "click ._client_userDetails__userDetails__detailsSectionA button": function(e) {
         e.preventDefault();
         Session.set('gender', e.target.name);
         $(userSections[0]).removeClass('fadeInRight');
@@ -27,9 +31,8 @@ Template.userDetails.events({
             $(userSections[0]).css("display", "none");
             $(userSections[1]).css("display", "block");
         }, 1000);
-        
     },
-     "click ._client_story__story__detailsSectionB button": function(e) {
+     "click ._client_userDetails__userDetails__detailsSectionB button": function(e) {
         e.preventDefault();
         Session.set('ageGroup', e.target.name);
         $(userSections[1]).removeClass('fadeInRight');
@@ -39,11 +42,15 @@ Template.userDetails.events({
             $(userSections[2]).css("display", "block");
         }, 1000);
     },
-     "click ._client_story__story__detailsSectionC button": function(e) {
+     "click ._client_userDetails__userDetails__detailsSectionC button": function(e) {
         e.preventDefault();
         Session.set('workingStatus', e.target.name);
         $(userSections[2]).removeClass('fadeInRight');
         $(userSections[2]).addClass('fadeOutLeft');
+        Session.set('userProfileDone', true);
+        window.setTimeout(function(){
+            $('.userMain').css("display","none");
+        }, 500);
     }
 });
 
